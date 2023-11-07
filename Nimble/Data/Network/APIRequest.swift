b19c4e07a3121954bb431e447edd4c44d9e0331d
Javier Pineda <jpgpineda98@gmail.com>
2023-11-06 23:44:03 -0600

    creating network layer

---
+++ Nimble/Data/Network/APIRequest.swift
@@ -0,0 +1,12 @@
+//
+//  APIRequest.swift
+//  Nimble
+//
+//  Created by javier pineda on 06/11/23.
+//
+
+import Foundation
+
+protocol APIRequest {
+    var apiRequest: URLRequest? { get }
+}
---
+++ Nimble/Data/Network/ApiRequestDispatcher.swift
@@ -0,0 +1,46 @@
+//
+//  ApiRequestDispatcher.swift
+//  Nimble
+//
+//  Created by javier pineda on 06/11/23.
+//
+
+import Foundation
+
+class ApiRequestDispatcher {
+    class func request<T: Codable>(apiRouter: ApiRouter) async throws -> T {
+        var components = URLComponents()
+        components.host = apiRouter.host
+        components.path = apiRouter.path
+        
+        guard let url = components.url else { throw APIRequestError.badUrl }
+        
+        var urlRequest = URLRequest(url: url)
+        urlRequest.httpMethod = apiRouter.method
+        if let body = apiRouter.body {
+            urlRequest.httpBody = body
+        }
+        let session = URLSession(configuration: .default)
+        return try await withCheckedThrowingContinuation { continuation in
+            let dataTask = session.dataTask(with: urlRequest) { data, response, error in
+                if let error = error {
+                    return continuation.resume(with: .failure(error))
+                }
+                
+                guard let data = data else {
+                    return continuation.resume(with: .failure(APIRequestError.noData))
+                }
+                
+                do {
+                    let responseObject = try JSONDecoder().decode(T.self, from: data)
+                    DispatchQueue.main.async {
+                        return continuation.resume(with: .success(responseObject))
+                    }
+                } catch {
+                    return continuation.resume(with: .failure(error))
+                }
+            }
+            dataTask.resume()
+        }
+    }
+}
